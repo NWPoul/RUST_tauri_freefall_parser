a092b867ffccf41e85c14302a44b840015070acb
Pavel <nwpoul@yandex.ru>
2024-07-26 21:09:10 +0300

    fix error with handling unicode chars

--- src-tauri/src/utils/u_serv.rs
+++ src-tauri/src/utils/u_serv.rs
@@ -49,14 +49,10 @@ pub fn normalize_name(t_name: &str) -> String {
     let lowercased = t_name.trim().to_lowercase().replace(" ", "_");
     let mut chars = lowercased.chars();
     let first_char = chars.next().unwrap_or('X').to_uppercase();
-    let mut remaining_chars: String = if lowercased.len() >= 2 {
-        chars.collect::<Vec<char>>().into_iter().collect::<String>()
-    } else {
-        "".to_string()
-    };
-    if remaining_chars.len() > MAX_NAME_LENGTH {
-        remaining_chars = remaining_chars[..=MAX_NAME_LENGTH].into();
-    }
+    let remaining_chars: String = chars
+        .take(MAX_NAME_LENGTH)
+        .collect::<Vec<char>>().into_iter().collect::<String>();
+
     let result = format!("{0}{1}", first_char, remaining_chars);
 
     result
