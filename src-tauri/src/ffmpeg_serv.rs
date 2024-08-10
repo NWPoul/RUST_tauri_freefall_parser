292e9c577f1698d1dd86d4c4156f90c010cc0351
Pavel <nwpoul@yandex.ru>
2024-08-10 15:18:54 +0300

    minor ref

--- src-tauri/src/ffmpeg_serv.rs
+++ src-tauri/src/ffmpeg_serv.rs
@@ -15,7 +15,7 @@ use std::{
 
 
 
-pub const GLITCH_MARGIN: f64 = 3.0;
+pub const GLITCH_MARGIN: f64 = 2.0;
 
 
 
@@ -57,7 +57,6 @@ pub fn run_ffmpeg(
     let (mut start_time, end_time) = target_start_end_time;
     let (src_file_path, dest_file_path) = src_dest_files_path;
 
-
     let glitch_margin:f64 = if start_time >= GLITCH_MARGIN {
         GLITCH_MARGIN
     } else {
@@ -69,23 +68,20 @@ pub fn run_ffmpeg(
     let ffmpeg_path = check_get_ffmpeg(ffmpeg_dir_path)?
         .display().to_string();
 
-
     let mut ffmpeg_status = Command::new(&ffmpeg_path)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
-
         .arg("-hide_banner")
+        
         .arg("-ss").arg(start_time.to_string())
         .arg("-to").arg(end_time.to_string())
         .arg("-i" ).arg(src_file_path)
         .arg("-ss").arg(glitch_margin.to_string())
-        .arg("-c")
-        .arg("copy")
+        .arg("-codec").arg("copy")
         .arg(dest_file_path)
         .arg("-n")
         .spawn()?;
-
 
     match ffmpeg_status.wait() {
         Ok(_)      => Ok(dest_file_path.clone()),
