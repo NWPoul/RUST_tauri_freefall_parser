141899121852cf6df5fda66b45fe28680abf83de
Pavel <nwpoul@yandex.ru>
2024-08-11 08:13:59 +0300

    mref (clean)

--- src/customHooks/index.ts
+++ src/customHooks/index.ts
@@ -1,26 +1,11 @@
 import { useDebounce }               from './useDebounce'
 import { useThrottle }               from './useThrottle'
 import {useKeypress, useKeyCodemap}  from './useKeypress'
-import {
-    useLongPress,
-    useRepeatableLongPress,
-    LongPressEventType,
-    type T_repeatableLongPressEvent,
-    type T_longPressEventHandlers,
-}                                    from './useRepeatableLongPress'
-// import {useBus, evDispatch}         from './useBus'
 
 
 export {
 	useKeypress,
 	useKeyCodemap,
-	// useBus, evDispatch,
-    useLongPress,
-    useRepeatableLongPress,
-    LongPressEventType,
-    type T_repeatableLongPressEvent,
-    type T_longPressEventHandlers,
-
     useDebounce,
     useThrottle,
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -9,68 +9,8 @@ function secTo_MMSS_String(sec: number, mmFlag = false) {
 }
 
 
-type T_setFlasherProps = {
-    remainingTime: number,
-    setPoint     ?: number,
-    flashSoftMode?: boolean,
-    targetNode   ?: HTMLElement,
-}
-const setFlasher = ({
-    remainingTime,
-    setPoint      = 10,
-    flashSoftMode = false,
-    targetNode    = document.body
-}:T_setFlasherProps) => {
-    const toggle = remainingTime < setPoint && remainingTime > 0
-    let alarmClassName, getBlinkDuration
-
-    switch (flashSoftMode) {
-        case true:
-            alarmClassName = 'alarmSoft'
-            getBlinkDuration = (remainingTime: number) => (
-                remainingTime > 5 ? 1 :
-                    remainingTime > 2 ? 0.5 : 0.25
-            )
-            break
-        default:
-            alarmClassName = 'alarmHard';
-            getBlinkDuration = (remainingTime: number) => (
-                remainingTime > 5 ? 1 :
-                    remainingTime > 2 ? 0.5 : 0.25
-            )
-    }
-
-    if (toggle) {
-        let blinkDuration = getBlinkDuration(remainingTime)
-        targetNode.style.setProperty('--blink-duration', `${blinkDuration}s`)
-        targetNode.classList.add(alarmClassName)
-    } else {
-        targetNode.classList.remove(alarmClassName)
-    }
-}
-
-
-// export function _i32_to_i16neg(value: number) {
-//     const max_i16 = 32767
-//     const i16neg = value >= 0
-//         ? value
-//         : value - max_i16
-
-//     console.log("🚀 ~ _i32_to_i16neg ", value, (i16neg - max_i16 - 1))
-    
-//     return i16neg
-// }
-
-
-
-
 export {cx} from './cx'
 
 export {
     secTo_MMSS_String,
-    setFlasher,
-}
-
-export type {
-    T_setFlasherProps,
 }
